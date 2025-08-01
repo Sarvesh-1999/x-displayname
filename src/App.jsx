79faa112b52d2fd3399743b26fd8c969eb3b2292
Sarvesh <sarvesh.kumar.jsr@gmail.com>
2025-08-01 13:17:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+  });
+
+  const [user,setUser] = useState("")
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser(formData.firstname + " " + formData.lastname);
+  };
+
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstname">First Name:</label>
+        <input
+          type="text"
+          name="firstname"
+          id="firstname"
+          value={formData.firstname}
+          onChange={handleChange}
+        />
+        <br />
+        <label htmlFor="lastname">Last Name:</label>
+        <input
+          type="text"
+          name="lastname"
+          id="lastname"
+          value={formData.lastname}
+          onChange={handleChange}
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+
+      {user && (
+        <div style={{ marginTop: "20px" }}>
+          <p>Full Name: {user}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
